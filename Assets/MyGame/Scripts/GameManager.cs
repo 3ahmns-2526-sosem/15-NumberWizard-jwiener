5d1890b9e0778d1095b89e656223055f0d826d9d
Jakob <jwiener@MacBook-Pro-von-Jakob-3.local>
2026-06-10 15:08:00 +0200

    feat: add correct answer state close #6

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +8,10 @@ public class GameManager : MonoBehaviour
     public int max;
     public int guess;
     public TextMeshProUGUI guessText;
+    public GameObject winPanel;
+    public Button higherButton;
+    public Button lowerButton;
+    public Button correctButton;
 
     private void Start()
     {
@@ -23,6 +28,14 @@ public class GameManager : MonoBehaviour
     {
         max = guess - 1;
         guess = (min + max) / 2;
+    }
+
+    public void Correct()
+    {
+        higherButton.interactable = false;
+        lowerButton.interactable = false;
+        correctButton.interactable = false;
+        winPanel.SetActive(true);
     }
 
     private void Update()
